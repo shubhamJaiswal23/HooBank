6b5f206a12a1f1a457a3307c5d52d58a47e210ec
Shubham Jaiswal <jaiswalshubham31012000@gmail.com>
2023-09-27 22:18:15 +0530

    change links url

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
-import styles from "../style";
-import { logo } from "../assets";
-import { footerLinks, socialMedia } from "../constants";
+import styles from '../style';
+import { logo } from '../assets';
+import { footerLinks, socialMedia } from '../constants';
 
 const Footer = () => (
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
@@ -18,7 +18,10 @@ const Footer = () => (
 
       <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
         {footerLinks.map((footerlink) => (
-          <div key={footerlink.title} className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}>
+          <div
+            key={footerlink.title}
+            className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}
+          >
             <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
               {footerlink.title}
             </h4>
@@ -27,7 +30,7 @@ const Footer = () => (
                 <li
                   key={link.name}
                   className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
-                    index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
+                    index !== footerlink.links.length - 1 ? 'mb-4' : 'mb-0'
                   }`}
                 >
                   {link.name}
@@ -41,7 +44,7 @@ const Footer = () => (
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Copyright Ⓒ 2022 HooBank. All Rights Reserved.
+        Copyright Ⓒ 2023 HooBank. All Rights Reserved.
       </p>
 
       <div className="flex flex-row md:mt-0 mt-6">
@@ -51,7 +54,7 @@ const Footer = () => (
             src={social.icon}
             alt={social.id}
             className={`w-[21px] h-[21px] object-contain cursor-pointer ${
-              index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+              index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0'
             }`}
             onClick={() => window.open(social.link)}
           />
